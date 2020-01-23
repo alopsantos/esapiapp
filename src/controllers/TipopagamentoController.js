a5cac617db4ea61752082c85dc951ad90ad4783c
alopsantos <anderson_lops@hotmail.com>
2020-01-23 18:45:25 -0300

    feature: Desenvolvido a feature da api tipopagamento

---
+++ src/controllers/TipopagamentoController.js
@@ -0,0 +1,36 @@
+const Tipopagamento = require('../models/Tipopagamento');
+
+module.exports = {
+    async store(require, response){
+        const { descricao } = require.body;
+        const tipopagamento = await Tipopagamento.create({descricao});
+        return response.json({tipopagamento});
+    },
+    async index(require, response){
+        const tipopagamento = await Tipopagamento.findAll();
+        return response.json({tipopagamento});
+    },
+    async update(require, response){
+        const { tipopagamento_id } = require.params;
+        const tipopagamento = await Tipopagamento.findByPk(tipopagamento_id);
+
+        if(!tipopagamento){
+            return response.status(400).json({ Atenção: 'Tipo de pagamento não encontrado!' });
+        }
+
+        const { descricao } = await tipopagamento.update(require.body);
+
+        return response.json({ tipopagamento_id, descricao });
+    },
+    async delete(require, response){
+        const { tipopagamento_id } = require.params;
+        const tipopagamento = await Tipopagamento.findByPk(tipopagamento_id);
+
+        if(!tipopagamento){
+            return response.status(400).json({ Atenção: 'Tipo de pagamento não encontrado' });
+        }
+
+        await tipopagamento.destroy();
+        return response.status(200).json({ Atenção: 'Tipo de pagamento excluido com sucesso!' });
+    }
+}
--- src/database/index.js
+++ src/database/index.js
@@ -3,11 +3,13 @@ const dbConfig = require('../config/database');
 
 const Pessoa = require('../models/Pessoa');
 const Movimentacao = require('../models/Movimentacao');
+const Tipopagamento = require('../models/Tipopagamento');
 
 const connection = new Sequelize(dbConfig);
 
 Pessoa.init(connection);
 Movimentacao.init(connection);
+Tipopagamento.init(connection);
 
 Movimentacao.associate(connection.models);
 
